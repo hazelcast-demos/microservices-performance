787e6102d7689d34707295c1cd1e74346d06bfb4
Nicolas Frankel <nicolas@frankel.ch>
2019-08-26 16:55:55 +0200

    Initial commit

---
+++ src/main/java/com/hazelcast/imdg/microservices/PersonController.java
@@ -0,0 +1,29 @@
+package com.hazelcast.imdg.microservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/d")
+public class PersonController {
+
+    private final PersonRepository repo;
+
+    public PersonController(PersonRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping("/")
+    public List<Person> getAllPersons() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id") Long id) {
+        return repo.findById(id).orElse(null);
+    }
+}
