70fa7036e5d2bc147ad062621a31c14e86d05903
Nicolas Frankel <nicolas@frankel.ch>
2019-08-26 16:55:55 +0200

    Initial commit

---
+++ src/main/java/org/hazelcast/microservices/HazelcastDemo.java
@@ -0,0 +1,12 @@
+package org.hazelcast.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazelcastDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HazelcastDemo.class, args);
+    }
+}
