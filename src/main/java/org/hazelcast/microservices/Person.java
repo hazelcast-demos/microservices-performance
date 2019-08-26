fb6b2b7d898dc0b9711c72688c449996fc08d368
Nicolas Frankel <nicolas@frankel.ch>
2019-08-26 16:55:55 +0200

    Initial commit

---
+++ src/main/java/org/hazelcast/microservices/Person.java
@@ -0,0 +1,63 @@
+package org.hazelcast.microservices;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Cache(region = "database", usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+
+    public Person() {
+    }
+
+    public Person(long id, String firstName, String lastName, LocalDate birthdate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+}
